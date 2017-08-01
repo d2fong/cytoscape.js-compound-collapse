f188ab77893f0a9ff1b56f252e83c3a83f2d1626
d2fong <d2fong@uwaterloo.ca>
2017-07-31 21:00:14 -0400

    Collapse mostly working; need to properly store meta edges original endpoints to restore on collapse

--- src/collection/collapse.js
+++ src/collection/collapse.js
@@ -4,33 +4,29 @@ const util = require('./util');
 // i.e all edges coming/going from the collapsed node's children 
 // will now point to the collapsed node itself
 const collapseEdges = (collapsedNode) => {
-  const collapsedCollection = collapsedNode.union(collapsedNode.descendants());
+  const descendants = collapsedNode.descendants();
+  const metaEdgeCandidates = descendants.connectedEdges();
 
-  const outgoerEdges = collapsedCollection.outgoers();
-  const incomerEdges = collapsedCollection.incomers();
-  const connectingEdges = outgoerEdges.union(incomerEdges);
+  metaEdgeCandidates.forEach((edge) => {
+    if (!descendants.contains(edge.source())) {
+      edge.move({
+        target: collapsedNode.id() 
+      });
+      edge.addClass('compoundcollapse.meta-edge');
+    }
 
-  connectingEdges.filter((edge) => !util.isMetaEdge(edge)).forEach((edge) => {
-    edge.addClass('meta-edge');
-    edge.scratch('_original-endpoints', {source: edge.source(), target: edge.target()});
-  });
-
-  incomerEdges.forEach((incomerEdge) => {
-    incomerEdge.move({
-      target: collapsedNode.id()
-    });
-  });
-
-  outgoerEdges.forEach((outgoerEdge) => {
-    outgoerEdge.move({
-      source: collapsedNode.id()
-    });
+    if (!descendants.contains(edge.target())) {
+      edge.move({
+        source: collapsedNode.id()
+      });
+      edge.addClass('compoundcollapse.meta-edge');
+    }
   });
 };
 
 const collapse = (node, opts) => {
   node.trigger('compoundcollapse.before-collapse');
-  node.data('compoundcollapse.size-before', node.layoutDimensions());
+  node.data('compoundcollapse.size-before', node.layoutDimensions({}));
 
   collapseEdges(node);
 
@@ -38,23 +34,7 @@ const collapse = (node, opts) => {
   
   node.data('compoundcollapse.collapsed-collection', collapsedCollection);
   collapsedCollection.remove();
-  node.data('compoundcollapse.size-after', node.layoutDimensions());
-
-  if (opts.repositionNodesAfter === 'both' || opts.repositionNodesAfter === 'collapse') {
-    const sizeDiff = {
-      x: ( node.data('compoundcollapse.size-before').w - node.data('compoundcollapse.size-after').w ) / 2,
-      y: ( node.data('compoundcollapse.size-before').h - node.data('compoundcollapse.size-after').h ) / 2
-    };
-    const siblings = util.sameDepthNodesRelativeTo(this.cy, node);
-    const nodePos = node.position();
-    siblings.forEach((sibling) => {
-      const sibPos = sibling.position();
-      sibling.position({
-        x: sibPos.x > nodePos.x ? sibPos.x - sizeDiff.x : sibPos.x + sizeDiff.x,
-        y: sibPos.y > nodePos.y ? sibPos.y - sizeDiff.y : sibPos.y + sibPos.y 
-      });
-    });
-  }
+  node.data('compoundcollapse.size-after', node.layoutDimensions({}));
 
   node.trigger('compoundcollapes.after-collapse');
 };
--- src/collection/index.js
+++ src/collection/index.js
@@ -1,3 +1,5 @@
+const collapseImpl = require('./collapse');
+
 const api = {
   isCollapsed (node) {
     return node.data('compound.collapse-collapsed');
@@ -5,13 +7,15 @@ const api = {
   isExpanded (node) {
     return !this.isCollapsed(node);
   },
-  collapse (eles) {
-  },
-  expand (eles) {
-  },
   isMetaEdge (edge) {
     return edge.hasClass('compound.collapse-meta-edge');
+  },
+  collapse (eles) {
+    eles.forEach(ele => collapseImpl(eles));
+  },
+  expand (eles) {
   }
+
 };
 
 module.exports = api;

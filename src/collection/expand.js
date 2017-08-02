23ea75050eb928cbf2f32fb7949a7413c262c9e1
d2fong <d2fong@uwaterloo.ca>
2017-08-01 21:15:48 -0400

    Remove layout after expanding for now

--- src/collection/expand.js
+++ src/collection/expand.js
@@ -38,29 +38,6 @@ const expand = (node, opts) => {
   node.data('compoundcollapse.collapsed-collection').restore();
   node.removeData('compoundcollapse.collapsed-collection');
 
-  // if (opts.layout) {
-  //   const layout = node.children().layout(opts.layout).run();
-  //   layout.pon('layoutstop').then((evt) => {
-  //     const curSize = node.layoutDimensions();
-  //     const prevSize = node.data('compoundcollapse.size-after');
-
-
-  //     const sizeDiff = {
-  //       x: ( curSize.w - prevSize.w )  / 2,
-  //       y: ( curSize.h - prevSize.h ) / 2
-  //     };
-  //     const nodePos = node.position();
-  //     const siblings = util.sameDepthNodesRelativeTo(siblings);
-
-  //     siblings.forEach((sibling) => {
-  //       const sibPos = sibling.position();
-  //       sibling.position({
-  //         x: sibPos.x > nodePos.x ? sibPos.x + sizeDiff.x : sibPos.x - sizeDiff.x,
-  //         y: sibPos.y > nodePos.y ? sibPos.y + sizeDiff.y : sibPos.y - sibPos.y 
-  //       });
-  //     });
-  //   });
-  // }
   node.trigger('compoundcollapse.after-expand');
 
 };
